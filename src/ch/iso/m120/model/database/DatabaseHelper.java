63ba7304035919da7c9e766d4cc81b86333f7bc4
micmine <micmine4@gmail.com>
2020-03-04 11:39:58 +0100

    Add save

--- src/ch/iso/m120/model/database/DatabaseHelper.java
+++ src/ch/iso/m120/model/database/DatabaseHelper.java
@@ -15,25 +15,19 @@ public class DatabaseHelper {
 
 	public <T> HashMap<String, String> selectOne(String query, Class<T> type) {
 		try {
-
 			Statement stmt = Database.getDatabaseConnection().createStatement();
-
 			ResultSet rs = stmt.executeQuery(query);
-
 			rs.next();
-
+			
 			HashMap<String, String> newObject = new HashMap<>();
-
 			Field[] fields = type.getDeclaredFields();
 			for (Field field : fields) {
 				newObject.put(field.getName(), rs.getString(field.getName()));
 			}
-
+			
 			rs.close();
 			stmt.close();
-
 			return newObject;
-
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -73,8 +67,48 @@ public class DatabaseHelper {
 	}
 
 	public <T> void save(Object object) {
-		System.out.println(this.getUpdateQuery(object));
-		System.out.println(this.getInsetQuery(object));
+		String query = "";
+		int id = 0;
+		try {
+			id = ((SimpleIntegerProperty) object.getClass().getField("id").get(object)).intValue();
+		} catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException | SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (this.exists(id, object.getClass())) {
+			query = this.getUpdateQuery(object);
+		} else {
+			query = this.getInsetQuery(object);
+		}
+		
+		System.out.println(query);
+	}
+	
+	private <T> boolean exists(int id, Class<T> type) {
+		String tableName = type.getSimpleName().toLowerCase();
+		
+		String query = "select count(*) from " + tableName + " where id = " + id;
+		try {
+			
+			Statement stmt = Database.getDatabaseConnection().createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			rs.next();
+			
+			int count = rs.getInt("count");
+			
+			rs.close();
+			stmt.close();
+
+			if (count >= 1) {
+				return true;
+			} else {
+				return false;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
 	}
 
 	private <T> String getInsetQuery(Object object) {
