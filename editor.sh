e0441fb3f54b0a6432ae5684c6df6aa4b552700a
micmine <micmine4@gmail.com>
2020-04-06 09:22:17 +0200

    fix editor.sh

---
+++ editor.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+SESSION="supportme-native"
+SESSIONEXISTS=$(tmux list-sessions | grep $SESSION)
+
+if [ "$SESSIONEXISTS" = "" ]
+then
+	tmux new-session -d -s $SESSION
+
+	tmux new-window -t $SESSION:1 -n 'edit'
+	tmux send-keys -t 'edit' 'nvim' C-m
+
+	tmux new-window -t $SESSION:2 -n 'run'
+	tmux send-keys -t 'run' ". ~/.bashrc && gradle" C-m
+
+	tmux new-window -t $SESSION:3 -n 'shell'
+	tmux send-keys -t 'shell' ". ~/.bashrc" C-m
+
+	tmux kill-window -t 0
+fi
+
+tmux attach-session -t $SESSION:1
